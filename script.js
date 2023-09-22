38d8a44fce241198d6974e9c7249f3ca5cf12a55
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2023-09-22 18:44:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+
+const tab=document.getElementById("sampleTable");
+
 function insert_Row() {
     //Write your code here
-  
+    let a=document.createElement("tr");
+    a.innerHTML="<td>item1</td><td>item2</td>";
+    tab.appendChild(a);
   
 }
